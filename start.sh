55d6b342eb99cce83690662d4ba71f44875efafa
Anders Norrback Bornholm <anders@bornholm.se>
2020-06-25 14:55:00 +0200

    SO close, just a little too much http

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "$REACT_APP_API_BASE_URL"
 
 for i in /usr/share/nginx/html/static/js/*.js; do
-    sed -i "s+http://replace-this-url+http://$REACT_APP_API_BASE_URL+g" $i
+    sed -i "s+http://replace-this-url+$REACT_APP_API_BASE_URL+g" $i
 done
 
 nginx -g "daemon off;"
