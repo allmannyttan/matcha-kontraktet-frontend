e962da1f0f6d4713486393c62117fa830d5ae374
Anders Norrback Bornholm <anders@bornholm.se>
2021-03-22 15:12:53 +0100

    Username label change

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import {
   Form,
   Input,
   Button,
   ErrorMessage,
-} from '../../components/FormElements'
-import styled from 'styled-components'
-import { post } from '../../utils/fetch'
-import history from '../../utils/history'
-import { connect } from 'react-redux'
-import { login } from '../../store/system/actions'
+} from "../../components/FormElements";
+import styled from "styled-components";
+import { post } from "../../utils/fetch";
+import history from "../../utils/history";
+import { connect } from "react-redux";
+import { login } from "../../store/system/actions";
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 const FormWrapper = styled.div`
   text-align: center;
   width: 100%;
   max-width: 300px;
   margin-top: 30px;
-`
+`;
 
 const H1 = styled.h1`
   font-size: 24px;
-`
+`;
 
 const LoginSchema = Yup.object().shape({
-  username: Yup.string().required('Obligatorisk'),
-  password: Yup.string().required('Obligatorisk'),
-})
+  username: Yup.string().required("Obligatorisk"),
+  password: Yup.string().required("Obligatorisk"),
+});
 
 interface LoginProps {
-  login: typeof login
+  login: typeof login;
 }
 
 const Login: React.FC<LoginProps> = ({ login }) => {
@@ -44,18 +44,18 @@ const Login: React.FC<LoginProps> = ({ login }) => {
       <FormWrapper>
         <H1>Logga in</H1>
         <Formik
-          initialValues={{ username: '', password: '' }}
+          initialValues={{ username: "", password: "" }}
           validationSchema={LoginSchema}
           onSubmit={async (input, { setFieldError }) => {
             try {
-              const response = await post('/auth/generate-token', input)
+              const response = await post("/auth/generate-token", input);
 
               login({
                 token: response.token,
-              })
-              history.push('/')
+              });
+              history.push("/");
             } catch (err) {
-              setFieldError('password', 'Inloggning misslyckades')
+              setFieldError("password", "Inloggning misslyckades");
             }
           }}
         >
@@ -64,7 +64,7 @@ const Login: React.FC<LoginProps> = ({ login }) => {
               id="username"
               name="username"
               type="text"
-              placeholder="Användarnamn"
+              placeholder="Användarnamn (demo)"
             />
             <ErrorMessage component="div" name="username" />
             <Input
@@ -79,7 +79,7 @@ const Login: React.FC<LoginProps> = ({ login }) => {
         </Formik>
       </FormWrapper>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default connect(null, { login })(Login)
+export default connect(null, { login })(Login);
