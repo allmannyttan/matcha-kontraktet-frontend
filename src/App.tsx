6d322c88f780444d1df46f84372cf8ef3d72c664
Rickard Natt och Dag <rickard@hey.com>
2021-04-20 11:46:02 +0200

    chore: remove create-react-app default styling and react logo (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import './App.css'
-import { Router } from 'react-router-dom'
-import App from './routes/App'
-import history from './utils/history'
-import { PersistGate } from 'redux-persist/lib/integration/react'
-import { Provider } from 'react-redux'
-import { store, persistor } from './store'
+import React from "react";
+import { Router } from "react-router-dom";
+import App from "./routes/App";
+import history from "./utils/history";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store";
 
 const RootApp: React.FC = () => {
   return (
@@ -16,7 +15,7 @@ const RootApp: React.FC = () => {
         </PersistGate>
       </Provider>
     </Router>
-  )
-}
+  );
+};
 
-export default RootApp
+export default RootApp;
