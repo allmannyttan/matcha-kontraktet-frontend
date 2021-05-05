89ff68955c555cf97333319618e1d38b5a5160fb
Radu Achim <radu.achim@iteam.se>
2021-05-05 13:37:34 +0200

    fix: 🐛 invalidate cache key
    backend made a breaking change and we have no support for cache
    invalidation so we just reset the key

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,23 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import { systemReducer } from './system/reducers'
-import { selectionReducer } from './selection/reducers'
-import { contractReducer } from './contract/reducers'
-import thunk from 'redux-thunk'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { systemReducer } from "./system/reducers";
+import { selectionReducer } from "./selection/reducers";
+import { contractReducer } from "./contract/reducers";
+import thunk from "redux-thunk";
 
 export const rootReducer = combineReducers({
   system: systemReducer,
   selection: selectionReducer,
   contract: contractReducer,
-})
+});
 
 const persistConfig = {
-  key: 'root',
+  key: "matcha-kontraktet",
   storage,
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer, applyMiddleware(thunk))
-export const persistor = persistStore(store)
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
